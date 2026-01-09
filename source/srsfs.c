3dc03a301498f0854371e9059b2a36d998953e4f
deadxraver <409853@edu.itmo.ru>
2026-01-09 15:39:25 +0300

    function skeletons, to be filled with logic, compiles & doesnt fail kernel (for now)

--- source/srsfs.c
+++ source/srsfs.c
@@ -1,3 +1,4 @@
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
@@ -10,12 +11,35 @@ MODULE_DESCRIPTION("A simple FS kernel module");
 
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
 
+static struct file_system_type srsfs_fs_type;
+
+static struct dentry* srsfs_mnt(
+    struct file_system_type* fs_type, int flags, const char* token, void* data
+) {
+  // TODO:
+  return NULL;
+}
+
+static void srsfs_kill(struct super_block* sb) {
+  // TODO:
+}
+
+static void set_fs_params(void) {
+  srsfs_fs_type.name = "srsfs";
+  srsfs_fs_type.mount = srsfs_mnt;
+  srsfs_fs_type.kill_sb = srsfs_kill;
+  // TODO:
+}
+
 static int __init srsfs_init(void) {
   LOG("SRSFS joined the kernel\n");
+  set_fs_params();
+  register_filesystem(&srsfs_fs_type);
   return 0;
 }
 
 static void __exit srsfs_exit(void) {
+  unregister_filesystem(&srsfs_fs_type);
   LOG("SRSFS left the kernel\n");
 }
 
