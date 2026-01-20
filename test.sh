818c8848302b58e551e5f998e7219e3cd2cfaf97
Даниил <deadxraver@yandex.ru>
2026-01-20 17:23:16 +0300

    add tests to Makefile

--- test.sh
+++ test.sh
@@ -3,12 +3,14 @@ insmod source/srsfs.ko
 mkdir -p /mnt/srsfs
 mount -t srsfs todo /mnt/srsfs
 mkdir -p testdata/ && cd testdata/
-echo '' > f
-for i in {0..1030}; do echo 1 >> f; done
+cat /dev/random | dd count=1 > f
 cp f /mnt/srsfs/
 echo 'СЕЙЧАС ВНИМАТЕЛЬНО'
 diff f /mnt/srsfs/f
 echo 'diff не должен был ничего вывести'
+cat /dev/random | dd count=10 > ff
+cp ff /mnt/srsfs/
+diff ff /mnt/srsfs/ff
 cd ..
 umount /mnt/srsfs
 rmmod srsfs
