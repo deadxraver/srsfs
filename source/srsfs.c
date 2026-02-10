963e570990e6587f84f590385087b6428837e21d
Даниил <deadxraver@yandex.ru>
2026-02-10 18:32:38 +0300

    format + todo comment

--- source/srsfs.c
+++ source/srsfs.c
@@ -308,21 +308,21 @@ static int srsfs_unlink(struct inode* parent_inode, struct dentry* child_dentry)
   const char* name = child_dentry->d_name.name;
   ino_t root = parent_inode->i_ino;
   struct srsfs_file* file = NULL;
-  struct flist* list = &((struct srsfs_inode_info*)
-      parent_inode->i_private)->dir_content;
-  for (struct flist* node = flist_iterate(list, list);
-      node != NULL; node = flist_iterate(list, node)) {
-          file = node->content;
-          if (strcmp(file->name, name))
-              continue;
-          if (file->is_dir)
-              return -EISDIR;
-          flist_remove(list, file);
-          destroy_file(file);
-          kvfree(file);
-          file = NULL;
-          return 0;
-      }
+  struct flist* list = &((struct srsfs_inode_info*)parent_inode->i_private)->dir_content;
+  for (struct flist* node = flist_iterate(list, list); node != NULL;
+       node = flist_iterate(list, node)) {
+    file = node->content;
+    if (strcmp(file->name, name))
+      continue;
+    if (file->is_dir)
+      return -EISDIR;
+    flist_remove(list, file);
+    destroy_file(file);
+    kvfree(file);
+    // TODO: clean inode->i_private
+    file = NULL;
+    return 0;
+  }
   return -ENOENT;
 }
 
